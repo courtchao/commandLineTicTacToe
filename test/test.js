1d621f2c40eb2d7376d9cdbe14a6beefaeadd6d0
courtchao <couchao@ucdavis.edu>
2018-09-06 12:00:18 -0700

    Add example test

---
+++ test/test.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+
+//Test
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
+
+describe('Tic-Tac-Toe', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
